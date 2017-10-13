24b39488f2ae6c0fdcee29114d26dc1a365ef24b
Kévin Dunglas <dunglas@gmail.com>
2017-10-13 21:14:04 +0200

    Fix form CSRF test

--- src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/form_csrf.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/Fixtures/php/form_csrf.php
@@ -1,6 +1,11 @@
 <?php
 
 $container->loadFromExtension('framework', array(
+    'session' => array(
+        'storage_id' => 'session.storage.native',
+        'handler_id' => 'session.handler.native_file',
+    ),
+    'csrf_protection' => array('enabled' => true),
     'form' => array(
         'csrf_protection' => array(
             'enabled' => true,
