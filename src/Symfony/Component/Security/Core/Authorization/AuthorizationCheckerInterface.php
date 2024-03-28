ebd05260226538089543b80c335e971e3f1233c6
Robin Chalas <chalasr@users.noreply.github.com>
2024-03-28 08:08:44 +0100

    Update AuthorizationCheckerInterface.php

--- src/Symfony/Component/Security/Core/Authorization/AuthorizationCheckerInterface.php
+++ src/Symfony/Component/Security/Core/Authorization/AuthorizationCheckerInterface.php
@@ -19,12 +19,13 @@ namespace Symfony\Component\Security\Core\Authorization;
 interface AuthorizationCheckerInterface
 {
     /**
-     * Checks if the attributes are granted against the current authentication token and optionally supplied subject.
+     * Checks if the attributes are granted against the current 
+     * authentication token and optionally supplied subject.
      *
-     * @param mixed $attribute A single attribute to vote on (can be of any type, string and instance of Expression are supported by the core)
+     * @param mixed $attribute A single attribute to vote on
      * @param mixed $subject
      *
      * @return bool
      */
-    public function isGranted($attribute, $subject = null);
+    public function isGranted(mixed $attribute, mixed $subject = null): bool;
 }
